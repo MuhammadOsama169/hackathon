895b7ab3cf16e3af248b88b9915b50d3f7c21409
MuhammadOsama169 <m.osama169@gmail.com>
2023-09-19 20:53:08 +0300

    bug fix: deployment

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'replicate.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'replicate.delivery',
-      },
-      {
-        protocol: 'https',
-        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
     domains: ['img.clerk.com'],
   },
 };
